b980e3a550a74603171788c2bcf834dfc5165003
Michaël Lhomme <papylhomme@gmail.com>
2018-02-10 14:17:25 +0100

    Add basic support for message acks

--- lib/mysensors/gateway.ex
+++ lib/mysensors/gateway.ex
@@ -5,15 +5,15 @@ defmodule MySensors.Gateway do
 
   @moduledoc """
   A server handling communication with a [MySensors Gateway](https://www.mysensors.org/download/serial_api_20)
-
-  **TODO**
-
-  - handle acks
   """
 
 
   use GenServer, start: {__MODULE__, :start_link, []}
   require Logger
+
+
+  # Default timeout when waiting for a reply from the network
+  @ack_timeout 1000
 
 
   #########
@@ -22,7 +22,7 @@ defmodule MySensors.Gateway do
 
 
   @doc """
-  Start the server on the given serial device
+  Start the server
   """
   @spec start_link :: GenServer.on_start
   def start_link do
@@ -35,7 +35,7 @@ defmodule MySensors.Gateway do
   """
   @spec send_message(Message.t) :: :ok
   def send_message(message) do
-    GenServer.cast(__MODULE__, {:send, message})
+    GenServer.cast(__MODULE__, {:send_message, message})
   end
 
 
@@ -44,8 +44,39 @@ defmodule MySensors.Gateway do
   """
   @spec send_message(Types.id, Types.id, Types.command, Types.type, String.t, boolean) :: :ok
   def send_message(node_id, child_sensor_id, command, type, payload \\ "", ack \\ false) do
-    message = MySensors.Message.new(node_id, child_sensor_id, command, type, payload, ack)
-    GenServer.cast(__MODULE__, {:send, message})
+    MySensors.Message.new(node_id, child_sensor_id, command, type, payload, ack)
+    |> send_message
+  end
+
+
+  @doc """
+  Send a message to the MySensors network, waiting for an ack from the destination
+  """
+  @spec sync_message(Message.t, timeout) :: :ok | :timeout
+  def sync_message(message, timeout \\ @ack_timeout) do
+    task = Task.async(fn ->
+      :ok = GenServer.call(__MODULE__, {:sync_message, message})
+      receive do
+        v -> v
+      end
+    end)
+
+    case Task.yield(task, timeout) || Task.shutdown(task) do
+      {:ok, _result} ->
+        :ok
+      nil ->
+        :timeout
+    end
+  end
+
+
+  @doc """
+  Send a message to the MySensors network, waiting for an ack from the destination
+  """
+  @spec sync_message(Types.id, Types.id, Types.command, Types.type, String.t, timeout) :: :ok | :timeout
+  def sync_message(node_id, child_sensor_id, command, type, payload \\ "", timeout \\ @ack_timeout) do
+    MySensors.Message.new(node_id, child_sensor_id, command, type, payload, true)
+    |> sync_message(timeout)
   end
 
 
@@ -54,40 +85,84 @@ defmodule MySensors.Gateway do
   """
   @spec version() :: :ok
   def version do
-    :ok = GenServer.call(__MODULE__, {:version})
+    task = Task.async(fn ->
+      :ok = GenServer.call(__MODULE__, {:version})
+      receive do
+        v -> v
+      end
+    end)
+
+    case Task.yield(task, @ack_timeout) || Task.shutdown(task) do
+      {:ok, result} ->
+        {:ok, result}
+      nil ->
+        {:error, :timeout}
+    end
   end
 
 
-  #################
-  #   Internals   #
-  #################
+  #########################
+  # Server implementation #
+  #########################
 
 
   # Init
   def init(nil) do
     Phoenix.PubSub.subscribe MySensors.PubSub, "incoming"
-    {:ok, %{}}
+    {:ok, %{version_handlers: [], ack_handlers: []}}
   end
 
 
   # Handle version call
-  def handle_call({:version}, _from, state) do
-    {:reply, _send_message(0, 255, :internal, I_VERSION), state}
+  def handle_call({:version}, _from = {pid, _ref}, state) do
+    {:reply, _send_message(0, 255, :internal, I_VERSION), %{state | version_handlers: state.version_handlers ++ [pid]}}
   end
 
 
-  # Handle send message call
-  def handle_cast({:send, message}, state) do
-    _send_message(message)
+  # Handle sync_message call
+  def handle_call({:sync_message, message}, _from = {pid, _ref}, state) do
+    _send_message(%{message | ack: true})
+    {:reply, :ok, %{state | ack_handlers: state.ack_handlers ++ [{message, pid}]}}
+  end
+
+
+  # Handle send_message cast
+  def handle_cast({:send_message, message}, state) do
+    _send_message(%{message | ack: false})
     {:noreply, state}
+  end
+
+
+  # Handle incoming acks
+  def handle_info({:mysensors_incoming, message = %{ack: true}}, state) do
+    Logger.debug "Handling ack: #{message}"
+
+    case Enum.find_index(state.ack_handlers, fn {original, _handler} -> message == original end) do
+      nil -> {:noreply, state}
+      i ->
+        {{_original, handler}, handlers} = List.pop_at(state.ack_handlers, i)
+        if Process.alive?(handler), do: send(handler, message)
+        {:noreply, %{state | ack_handlers: handlers}}
+    end
+  end
+
+
+  # Handle version response
+  def handle_info({:mysensors_incoming, message = %{command: :internal, type: I_VERSION}}, state) do
+    Logger.debug "Handling version response: #{message}"
+
+    case state.version_handlers do
+      [] -> {:noreply, state}
+      [handler | handlers] ->
+        send handler, message.payload
+        {:noreply, %{state | version_handlers: handlers}}
+    end
   end
 
 
   # Handle incoming messages
   def handle_info({:mysensors_incoming, message}, state) do
-    #Logger.debug "Received message: #{message}"
     _process_message(message)
-
     {:noreply, state}
   end
 
@@ -99,10 +174,10 @@ defmodule MySensors.Gateway do
   end
 
 
-  # Process acks
-  defp _process_message(msg = %{ack: :true}) do
-    Logger.debug "Handling ack: #{inspect msg}"
-  end
+  #################
+  #   Internals   #
+  #################
+
 
 
   # Process a presentation message
@@ -190,10 +265,8 @@ defmodule MySensors.Gateway do
   defp _send_message(msg) do
     str = MySensors.Message.serialize(msg)
 
-    Logger.debug fn -> "Sending message #{msg}-> RAW: #{str}" end
+    Logger.debug fn -> "Sending message #{msg}" end
     Phoenix.PubSub.broadcast MySensors.PubSub, "outgoing", {:mysensors_outgoing, str}
   end
 
-
 end
-
