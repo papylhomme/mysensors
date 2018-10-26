00f9aa563e189793d983b9ea765602be61ecd09f
Michaël Lhomme <papylhomme@gmail.com>
2018-10-26 16:10:45 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MySensors.Mixfile do
     [
       app: :mysensors,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,11 +22,11 @@ defmodule MySensors.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:phoenix_pubsub, "~> 1.0"},
-      {:uuid, "~> 1.1"},
+      {:elixir_uuid, "~> 1.2"},
       {:nerves_uart, "~> 1.2"}, # required by SerialBridge
-      {:tortoise, "~> 0.2"}
+      {:tortoise, "~> 0.9"}
     ]
   end
 end
